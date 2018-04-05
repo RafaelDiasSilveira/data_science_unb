ea034e3a24ce8f26816d4a77dbcdac25c7a3f117
Rafael Dias <rafa197795@gmail.com>
2018-04-05 19:52:39 -0300

    Leitura e estruturação em dataframe do arquivo unb.relatorioOrientacao.json

--- scripts/rel_orientacao.R
+++ scripts/rel_orientacao.R
@@ -7,23 +7,26 @@ library(rlist)
 rel_orientacao_json <- "./files/unb.relatorioOrientacao.json"
 rel_orientacao_raw <- fromJSON(rel_orientacao_json)
 
+tipo_orientacao <- as.factor(names(rel_orientacao_raw))
+
 df_final <- data.frame()
+i <- 1
 
 # Loop para gerar o dataframe final
 for (lista in rel_orientacao_raw) {
   for (df in lista) {
+    df <- data.frame(tipo_orientacao[i], df)
     df_final <- rbind(df_final, df)
   }
+  i <- i + 1
 }
-
-# Definir quais são factors
-df_final$natureza <- as.factor(df_final$natureza) 
-df_final$ano <- as.factor(df_final$ano)
-df_final$instituicao <- as.factor(df_final$instituicao) 
-df_final$curso <- as.factor(df_final$curso) 
-df_final$bolsa <- as.factor(df_final$bolsa) 
-
-glimpse(df_final)
-summary(df_final$ano)
-
-plot(x = df_final$ano, y = df_final$bolsa)
+# 
+# # Definir quais são factors
+# df_final$natureza <- as.factor(df_final$natureza) 
+# df_final$ano <- as.factor(df_final$ano)
+# df_final$instituicao <- as.factor(df_final$instituicao) 
+# df_final$curso <- as.factor(df_final$curso) 
+# df_final$bolsa <- as.factor(df_final$bolsa) 
+# 
+# glimpse(df_final)
+# summary(df_final$ano)

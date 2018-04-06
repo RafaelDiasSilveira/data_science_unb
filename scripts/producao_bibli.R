ecad6ca9803e33a052425656a0a902a3fdbe9590
Rafael Dias <rafa197795@gmail.com>
2018-04-05 23:11:04 -0300

    Arquivo unb.relatorioProducaoBibiografica.json analisado

---
+++ scripts/producao_bibli.R
@@ -0,0 +1,28 @@
+library(jsonlite)
+library("dplyr")
+library(stringr)
+library(rlist)
+
+# Importação do arquivo JSON
+prod_bibli_json <- "./files/unb.relatorioProducaoBibiografica.json"
+prod_bibli_raw <- fromJSON(prod_bibli_json)
+
+# Verifica a estrutura do arquivo
+str(prod_bibli_raw, vec.len	= 0, max.level = 3)
+
+
+# Criação dos dataframes
+temp <- prod_bibli_raw$PERIODICO
+periodico_df <- do.call("rbind", temp)
+
+temp <- prod_bibli_raw$LIVRO
+livro_df <- do.call("rbind", temp)
+
+temp <- prod_bibli_raw$CAPITULO_DE_LIVRO
+capitulo_livro_df <- do.call("rbind", temp)
+
+temp <- prod_bibli_raw$TEXTO_EM_JORNAIS
+texto_jornal_df <- do.call("rbind", temp)
+
+temp <- prod_bibli_raw$ARTIGO_ACEITO
+artigo_df <- do.call("rbind", temp)
--- scripts/rel_orientacao.R
+++ scripts/rel_orientacao.R
@@ -20,13 +20,14 @@ for (lista in rel_orientacao_raw) {
   }
   i <- i + 1
 }
-# 
-# # Definir quais são factors
-# df_final$natureza <- as.factor(df_final$natureza) 
-# df_final$ano <- as.factor(df_final$ano)
-# df_final$instituicao <- as.factor(df_final$instituicao) 
-# df_final$curso <- as.factor(df_final$curso) 
-# df_final$bolsa <- as.factor(df_final$bolsa) 
-# 
-# glimpse(df_final)
+
+
+# Definir quais são factors
+df_final$natureza    <- as.factor(df_final$natureza)
+df_final$ano         <- as.factor(df_final$ano)
+df_final$instituicao <- as.factor(df_final$instituicao)
+df_final$curso       <- as.factor(df_final$curso)
+df_final$bolsa       <- as.factor(df_final$bolsa)
+
+glimpse(df_final)
 # summary(df_final$ano)
